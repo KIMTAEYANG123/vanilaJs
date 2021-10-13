b4b215849eef79221c0631777f9d11c476fcf9c0
TAEYANG <explore2012@naver.com>
2021-10-14 01:36:59 +0900

    delete localStorage and todos

--- js/todo.js
+++ js/todo.js
@@ -1,27 +1,31 @@
 const todoForm = document.querySelector('.todo-form');
 const todoInput = todoForm.querySelector('input');
 const todoList = document.querySelector('.todo-list');
+const TODOS_KEY = 'todos';
 
-
+// 투두리스트 삭제 함수
 function deleteTodo(e){
     const deleteLi = e.target.parentElement;
+    todos = todos.filter( item => (Number(deleteLi.className) !== item.id))
     deleteLi.remove();
+    saveTodo()
 }
 
+let todos = [];
 
-const todos = [];
-
+// 로컬스토리지에 등록
 function saveTodo(){
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 // li태그를 생성하여 작성한 내용을 기록 
 function paintTodo(newTodo){
     const todoLi = document.createElement('li');
+    todoLi.className = newTodo.id;
     const span = document.createElement('span');
     const button = document.createElement('button');
 
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     todoLi.appendChild(span);
 
     button.innerText ='X';
@@ -37,10 +41,24 @@ function handleTodoSubmit(e){
     e.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = '';
-    toods.push(newTodo)
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    }
+    todos.push(newTodoObj)
+    paintTodo(newTodoObj);
     saveTodo();
+}
+todoForm.addEventListener('submit',handleTodoSubmit)
+
+
+//로컬스토리지에 값을 가져오기
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parseToDos = JSON.parse(savedToDos);
+    todos = parseToDos;
+    parseToDos.forEach(paintTodo)
 }
 
 
-todoForm.addEventListener('submit',handleTodoSubmit)
